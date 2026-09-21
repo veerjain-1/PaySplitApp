d3121b6a8794c483838276ab7357b9dc63ee4834
Veer Jain <veer.spam.jain@gmail.com>
2026-09-21 18:08:19 -0400

    feat(backend): finish recurring expense model and add api routes

--- backend/src/models/RecurringExpense.js
+++ backend/src/models/RecurringExpense.js
@@ -21,10 +21,53 @@ const recurringExpenseSchema = new mongoose.Schema({
     enum: ['daily', 'weekly', 'monthly', 'yearly'],
     required: true
   },
-  // TODO: Add split logic (exact amounts, percentages, etc)
-  // TODO: Add nextBillingDate calculation logic
-  // TODO: Add active status toggle
+  splits: [{
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    },
+    amount: {
+      type: Number,
+      required: true,
+      min: [0, 'Split amount must be positive']
+    }
+  }],
+  nextBillingDate: {
+    type: Date,
+    required: true
+  },
+  isActive: {
+    type: Boolean,
+    default: true
+  }
 }, { timestamps: true });
 
-// TODO: Finish model and export
+// Pre-save hook to calculate next billing date if not set
+recurringExpenseSchema.pre('save', function(next) {
+  if (this.isModified('frequency') || !this.nextBillingDate) {
+    this.nextBillingDate = this.calculateNextDate(new Date());
+  }
+  next();
+});
+
+recurringExpenseSchema.methods.calculateNextDate = function(fromDate) {
+  const nextDate = new Date(fromDate);
+  switch (this.frequency) {
+    case 'daily':
+      nextDate.setDate(nextDate.getDate() + 1);
+      break;
+    case 'weekly':
+      nextDate.setDate(nextDate.getDate() + 7);
+      break;
+    case 'monthly':
+      nextDate.setMonth(nextDate.getMonth() + 1);
+      break;
+    case 'yearly':
+      nextDate.setFullYear(nextDate.getFullYear() + 1);
+      break;
+  }
+  return nextDate;
+};
+
 module.exports = mongoose.model('RecurringExpense', recurringExpenseSchema);
